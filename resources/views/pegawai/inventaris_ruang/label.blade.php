397dc41588aeb07956fdd44eb60eb794a50c2ba8
chairyandwi <chairiandwis@gmail.com>
2025-12-31 14:41:42 +0700

    refactor: Improve label layout and structure for better readability and consistency

--- resources/views/pegawai/inventaris_ruang/label.blade.php
+++ resources/views/pegawai/inventaris_ruang/label.blade.php
@@ -31,92 +31,167 @@ body {
     border-spacing: 4mm 4mm;
 }
 
-.sheet td {
+.sheet > tbody > tr > td {
     width: 33.33%;
 }
 
 /* ========== LABEL ========== */
 .label {
-    width: 6cm;
-    height: 2.5cm;
+    width: 60mm;
+    height: 25mm;
     border: 0.35mm solid #111;
 }
 
 /* ========== STRUKTUR LABEL ========== */
-.label-table {
+.label-cols {
     width: 100%;
     height: 100%;
-    border-collapse: collapse;
-    table-layout: fixed;
+    position: relative;
 }
 
-.label-table td {
-    vertical-align: middle;
+.label-cols::before,
+.label-cols::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 0.25mm;
+    background: #111;
+}
+
+.label-cols::before {
+    left: calc(12mm - 0.125mm);
+}
+
+.label-cols::after {
+    left: calc(42mm - 0.125mm);
+}
+
+.label-cols > .col-logo,
+.label-cols > .col-info,
+.label-cols > .col-cond {
+    height: 100%;
     text-align: center;
-    border-right: 0.25mm solid #111;
-}
-
-.label-table td:last-child {
-    border-right: none;
+    box-sizing: border-box;
+    position: absolute;
+    top: 0;
+    bottom: 0;
 }
 
 /* ========== KOLOM (FINAL) ========== */
 .col-logo {
-    width: 1.1cm;
+    width: 12mm;
+    left: 0;
+    display: block;
+    position: relative;
 }
 
 .col-info {
-    width: 4.1cm;
-    padding: 0.8mm 1.2mm;
+    width: 30mm;
+    left: 12mm;
+    padding: 0;
+    overflow: hidden;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
 }
 
 .col-cond {
-    width: 0.8cm;
+    width: 18mm;
+    left: 42mm;
     font-weight: bold;
+    display: block;
+    position: relative;
+    padding: 0;
+    box-sizing: border-box;
+    text-align: center;
+}
+
+.logo-wrap,
+.cond-wrap {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    text-align: center;
 }
 
 /* ========== LOGO ========== */
 .logo img {
-    width: 9mm;
-    height: 9mm;
+    width: 8mm;
+    height: 8mm;
     object-fit: contain;
 }
 
 /* ========== INFO INTERNAL ========== */
+.info-wrap {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+}
+
 .info-block {
     border-bottom: 0.25mm solid #111;
-    padding: 0.6mm 0;
+    padding: 0.4mm 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 33.33%;
+    text-align: center;
+    align-items: center;
 }
 
 .info-block:last-child {
     border-bottom: none;
+    padding-bottom: 0.2mm;
 }
 
 /* ========== TEKS ========== */
 .title {
     font-weight: bold;
     font-size: 6.8pt;
+    width: 100%;
+    text-align: center;
 }
 
 .value {
     font-weight: bold;
     font-size: 6.8pt;
-    white-space: nowrap;
+    white-space: normal;
+    overflow-wrap: break-word;
+    width: 100%;
+    text-align: center;
 }
 
 .small {
     font-size: 6pt;
     color: #444;
+    line-height: 1.1;
+    width: 100%;
+    text-align: center;
 }
 
 /* ========== KONDISI ========== */
 .cond-title {
-    font-size: 6pt;
+    font-size: 5.8pt;
+    line-height: 1.1;
+    width: 100%;
+    text-align: center;
 }
 
 .cond-value {
-    font-size: 6.6pt;
+    font-size: 6.2pt;
     font-weight: bold;
+    line-height: 1.1;
+    white-space: normal;
+    word-break: break-word;
+    width: 100%;
+    text-align: center;
 }
 </style>
 </head>
@@ -152,46 +227,48 @@ $status = (bool)$unit->kerusakanAktif ? 'Rusak' : 'Baik';
 
 <td>
 <div class="label">
-<table class="label-table">
-<tr>
+<div class="label-cols">
 
     <!-- LOGO -->
-    <td class="col-logo">
-        @if ($logoBase64)
-            <div class="logo">
-                <img src="{{ $logoBase64 }}">
-            </div>
-        @endif
-    </td>
+    <div class="col-logo">
+        <div class="logo-wrap">
+            @if ($logoBase64)
+                <div class="logo">
+                    <img src="{{ $logoBase64 }}">
+                </div>
+            @endif
+        </div>
+    </div>
 
     <!-- INFO -->
-    <td class="col-info">
+    <div class="col-info">
+        <div class="info-wrap">
+            <div class="info-block">
+                <div class="title">No Reg:</div>
+                <div class="value">{{ $unit->kode_unit }}</div>
+            </div>
 
-        <div class="info-block">
-            <div class="title">No Reg:</div>
-            <div class="value">{{ $unit->kode_unit }}</div>
+            <div class="info-block">
+                <div class="title">Barang:</div>
+                <div class="value">{{ $unit->barang?->nama_barang }}</div>
+                <div class="small">({{ $unit->ruang?->nama_ruang }})</div>
+            </div>
+
+            <div class="info-block">
+                <div class="small">Cek: {{ $cekTanggal }}</div>
+            </div>
         </div>
-
-        <div class="info-block">
-            <div class="title">Barang:</div>
-            <div class="value">{{ $unit->barang?->nama_barang }}</div>
-            <div class="small">({{ $unit->ruang?->nama_ruang }})</div>
-        </div>
-
-        <div class="info-block">
-            <div class="small">Cek: {{ $cekTanggal }}</div>
-        </div>
-
-    </td>
+    </div>
 
     <!-- KONDISI -->
-    <td class="col-cond">
-        <div class="cond-title">KONDISI</div>
-        <div class="cond-value">{{ $status }}</div>
-    </td>
+    <div class="col-cond">
+        <div class="cond-wrap">
+            <div class="cond-title">KONDISI</div>
+            <div class="cond-value">{{ $status }}</div>
+        </div>
+    </div>
 
-</tr>
-</table>
+</div>
 </div>
 </td>
 
